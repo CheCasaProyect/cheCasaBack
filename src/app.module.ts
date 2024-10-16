28f82fd880e31f5ac8d956d2b9584f6539c2c179
Ciin Noguera <ciin.noguera@hotmail.com>
2024-10-16 13:09:23 -0300

    Swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 import typeOrmConfig from './config/typeorm'; 
 
 
@@ -17,8 +20,9 @@ import typeOrmConfig from './config/typeorm';
     inject:[ConfigService],
     useFactory:(configService: ConfigService) => configService.get('typeorm'),
   }),
-    AuthModule],
-  controllers: [AuthController],
-  providers: [AuthService],
+    AuthModule,
+    UserModule],
+  controllers: [AuthController, UserController],
+  providers: [AuthService, UserService],
 })
 export class AppModule {}
---
+++ src/entities/user.entity.ts
@@ -0,0 +1,66 @@
+// import { ApiProperty } from "@nestjs/swagger";
+// import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+// import { v4 as uuid } from 'uuid'
+// import { UserRole } from "./user.enum";
+
+// @Entity ({
+//     name: 'users',
+// })
+// export class User {
+//     @PrimaryGeneratedColumn()
+//     @ApiProperty({
+//         description: 'User id',
+//         format: 'uuid',
+//         example: '550e8400-e29b-41d4-a716-446655440000'
+//        })
+//        id: string = uuid();
+
+//     @Column({type: 'varchar', length: 50, nullable: false})
+//     @ApiProperty({
+//         description: 'User firstname',
+//         example: 'CheCasa'
+//     }) 
+//     firstname: string;
+
+//     @Column({type: 'varchar', length: 50, nullable: false})
+//     @ApiProperty({
+//         description: 'User lastname',
+//         example: 'CheCasa'
+//     }) 
+//     lastname: string;
+
+//     @Column({})
+//     @ApiProperty({})
+//     birthdate: Date
+
+//     @Column({type: 'int'})
+//     @ApiProperty({
+//         description: 'Phone number',
+//         example: '5491122334455'
+//     })
+//     phone: number;
+
+//     @Column({type: 'varchar', length: 50, unique: true, nullable: false})
+//     @ApiProperty({
+//         description: 'User email',
+//         example: 'checasa@test.com'
+//     })
+//     email: string;
+
+//     @Column({type: 'varchar', length:128, nullable: false})
+//     @ApiProperty({
+//         description: 'User password',
+//         example: 'checasa123'
+//     })
+//     password: string;
+
+//     @Column({type: 'enum', enum: UserRole})
+//     @ApiProperty({
+//         description: ' User role',
+//         enum: UserRole,
+//         example: 'Traveler'
+//     })
+//     role: UserRole
+
+
+// }
