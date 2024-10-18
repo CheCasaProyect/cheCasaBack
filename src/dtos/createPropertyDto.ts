524f3001455385ae5ce3409db33dae51f520e712
nahuemarin <guatamarin@gmail.com>
2024-10-18 12:37:07 -0300

    cheCasaBack/Nahue/Properties_CRUD
    hice el CRUD básico para la entidad de propiedad, trabaje en el @Get, @Get(`id`) y @Post()

---
+++ src/dtos/createPropertyDto.ts
@@ -0,0 +1,60 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreatePropertyDto {
+  @ApiProperty({
+    description: 'Title, property type',
+    example: 'Cabaña',
+  })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    description: 'Property description',
+  })
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @ApiProperty({
+    description: 'Property location',
+  })
+  @IsNotEmpty()
+  @IsString()
+  location: string;
+
+  @ApiProperty({
+    description: 'Property price',
+    example: 1000,
+  })
+  @IsNotEmpty()
+  @IsInt()
+  price: number;
+
+  @ApiProperty({
+    description: 'Availability',
+  })
+  @IsNotEmpty()
+  @IsBoolean()
+  isAvailable: boolean;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsInt()
+  capacity: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsInt()
+  room: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsInt()
+  bathrooms: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  photos: File[];
+}
