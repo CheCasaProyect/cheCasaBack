04d341c2d65b0f55f93ad0a5648ef42f2fc12e10
lciambotti <lciambotti@hotmail.com>
2024-10-23 14:57:35 -0300

    agregue validadores a los DTOS de reservas

--- src/dtos/reservationDetailsDTO.ts
+++ src/dtos/reservationDetailsDTO.ts
@@ -1,12 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsInt, IsNotEmpty, IsNumber, IsString, IsUUID, Min } from "class-validator";
 
 
 export class ReservationDetailsDTO{
 
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsDate()
     checkIn: Date;
 
+    @ApiProperty()
+    @IsNotEmpty()
     checkOut: Date;
 
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    @IsInt()
+    @Min(1)
     pax: number;
 
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsUUID()
     propertyId: string;
 }
