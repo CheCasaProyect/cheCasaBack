03df9029039d0fc6c00abb199a14e200e7c9f8ec
lciambotti <lciambotti@hotmail.com>
2024-10-28 01:05:43 -0300

    agregar checkout de stripe y primer testing con front end

---
+++ src/dtos/createPaymentDto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty } from "class-validator";
+
+
+export class CreatePaymentDto {
+
+    @ApiProperty()
+    @IsNotEmpty()
+    stripeProductId: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    stripePriceId: string;
+}
