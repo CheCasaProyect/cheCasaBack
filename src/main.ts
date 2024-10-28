c5566d980dce7da40677391be367cf1ab36816d5
Ciin Noguera <ciin.noguera@hotmail.com>
2024-10-28 01:41:57 -0300

    Cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express'; 
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://checasafront.onrender.com',
+  });
+  
   app.useGlobalPipes(new ValidationPipe())
 
   setupSwagger(app);
