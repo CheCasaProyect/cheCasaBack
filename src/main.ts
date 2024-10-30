919a1660c01d7f0d14fe3bb6166e030d564bde6b
lciambotti <lciambotti@hotmail.com>
2024-10-30 13:43:38 -0300

    cors general

--- src/main.ts
+++ src/main.ts
@@ -7,26 +7,16 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: ['https://checasafront.onrender.com', 'https://localhost:3000'],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type, Authorization',
-  // });
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   
   app.useGlobalPipes(new ValidationPipe());
   
   setupSwagger(app);
-  
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();
-   });
-  
+
   await app.listen(5432);
 }
 
