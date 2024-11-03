7508278abdbd22fbad2a8485651b6ad883debb0d
Ciin Noguera <ciin.noguera@hotmail.com>
2024-11-03 19:08:33 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swaggerConfig';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
+import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors());
 
   /* app.use(cors()); */
   // app.enableCors({
@@ -23,18 +25,18 @@ async function bootstrap() {
 
   setupSwagger(app);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, DELETE',
-    );
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin, X-Requested-With, Content-Type, Accept',
+  //   );
+  //   res.header(
+  //     'Access-Control-Allow-Methods',
+  //     'GET, POST, OPTIONS, PUT, DELETE',
+  //   );
+  //   next();
+  // });
 
   await app.listen(3000);
 }
