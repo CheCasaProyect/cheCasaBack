8af24dd12732d4eef6728a4d2291a3165a6a27dc
nahuemarin <guatamarin@gmail.com>
2024-11-07 04:08:38 -0300

    Correccion del enpoint en reservations y agregue el guard para estas

--- src/dtos/createReservationDto.ts
+++ src/dtos/createReservationDto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Exclude, Type } from 'class-transformer';
-import { IsDate, IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsUUID } from 'class-validator';
 
 export class CreateReservationDTO {
   @ApiProperty()
--- src/property/property.module.ts
+++ src/property/property.module.ts
@@ -10,6 +10,9 @@ import { CloudinaryService } from 'src/files/cloudinary.service';
 import { HttpModule } from '@nestjs/axios';
 import { GeocodingService } from './geocodingService';
 import { StripeModule } from 'src/stripe/stripe.module';
+import { UserModule } from 'src/user/user.module';
+import { PropertyGuard } from 'src/guards/property.guard';
+import { UserRepository } from 'src/user/user.repository';
 
 @Module({
   imports: [
